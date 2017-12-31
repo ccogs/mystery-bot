3609ae124a9322cbbf1de92d04b462e4a41e7cab
ericrw96 <ericrw96@vt.edu>
2017-12-31 11:54:48 -0500

    Add in an initial attempt to looking at the news summary library.

---
+++ news_summarizer.js
@@ -0,0 +1,36 @@
+let summary = require('node-summary');
+
+
+let _getSummaryFromUrl = function (url) {
+    // convert summary.summarizeFromUrl to use promises instead of a callback
+    return new Promise(function (resolve, reject) {
+        summary.summarizeFromUrl(url, function (err, data) {
+            if (err !== null) return reject(err);
+            resolve(data);
+        });
+    });
+};
+
+let _lookupRandomArticle = function() {
+    // TODO make non random hardcoded URL
+    return 'https://www.nytimes.com/2017/12/29/dining/raw-water-unfiltered.html';
+};
+
+
+
+
+module.exports = {
+    entry: function (senderid, message, writeMessage) {
+        if(message === 'news'){
+            let url = _lookupRandomArticle();
+            let sum = _getSummaryFromUrl(url);
+            message = {
+                text : url +'\n\n' +  sum
+            };
+            writeMessage(senderid, message);
+            return true;
+        }
+        return false;
+    }
+
+};

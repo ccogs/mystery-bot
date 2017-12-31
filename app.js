d383aa84242cfb17070e701ac8296b7a3319a5f1
Christy Coghlan <ccogs@vt.edu>
2017-12-30 23:33:50 -0500

    requiring xmlhttprequest

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var request = require("request");
 var bodyParser = require("body-parser");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
